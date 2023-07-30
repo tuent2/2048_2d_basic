28d120fbc07fede700f7531f75c4046ec1e48f36
tuent2 <tuent2forunity@gmail.com>
2023-07-31 00:39:43 +0700

    first

---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "TileState", menuName = "2048_2d/TileState")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+}
+
